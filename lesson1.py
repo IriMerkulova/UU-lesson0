a20cb67c6f2641149b65356d6e9ec12d7c20bf51
IriMerkulova <151544565+IriMerkulova@users.noreply.github.com>
2024-10-16 13:19:52 +0300

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,22 @@
+print("1st program")
+print(9**0.5*5)
+
+
+print("2nd program")
+print(9.99>9.98 and 1000!=1000.1)
+
+
+print('3rd program')
+a= 1234//10%100
+b= 5678//10%100
+print(a+b)
+
+
+print('4th program')
+a=13.42
+b=42.13
+c=int(a)
+d=int(b)
+e=int(a%1*100)
+f=int(b%1*100)
+print (c==f or d==e)
